e785353d6d4af29fc3eb86b6c1c8199b44f0aeb6
carlo.bongiovanni@gmail.com <carlo.bongiovanni@gmail.com>
2023-06-06 17:32:42 +0200

    calendar with basic event handling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,11 +10,6 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
   </div>
 </template>
 
@@ -23,10 +18,6 @@ h1 {
   font-weight: 500;
   font-size: 2.6rem;
   top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
 }
 
 .greetings h1,
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import WaveUI from 'wave-ui'
+import 'wave-ui/dist/wave-ui.css'
 
 const app = createApp(App)
+app.use(WaveUI, { /* Some Wave UI options */ })
 
 app.use(router)
 
